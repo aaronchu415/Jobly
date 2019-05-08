46e35992bcb9f25f841aceb78304581061063b79
aaronchu415 <aaronchu415@gmail.com>
2019-05-08 11:07:47 -0700

    added test for company post route

--- __tests__/unit/companies.test.js
+++ __tests__/unit/companies.test.js
@@ -26,72 +26,95 @@ afterAll(async function () {
     await db.end();
 });
 
-describe("GET /companies", function () {
+// describe("GET /companies", function () {
 
-    test("Should return a the handle and name for all of the company objects.", async function () {
-        const response = await request(app)
-            .get("/companies");
-        expect(response.statusCode).toBe(201);
-        // expect(response.body.book).toHaveProperty("isbn")
-        // expect(response.body.book.isbn).toEqual("0691161789");
-    })
-})
-
-describe("DELETE /companies", function () {
-
-    test("Should remove an existing company and return a message => {message: 'Company deleted'}", async function () {
-        const response = await request(app)
-            .delete(`/companies/${testCompanyHandle}`);
-
-        //test status code and response   
-        expect(response.statusCode).toBe(201);
-        expect(response.body).toEqual({ message: 'Company deleted' })
-
-        //query DB for handle. Should not exist in db
-        let testHandleResult = await db.query(`
-        SELECT * from companies WHERE handle=$1
-        `, [testCompanyHandle]);
-        expect(testHandleResult.rows).toHaveLength(0);
-    })
-})
-
-// describe("POST /companies", function () {
-//     // success
-//     test("Create a valid new book", async function () {
+//     test("Should return a the handle and name for all of the company objects.", async function () {
 //         const response = await request(app)
-//             .post("/books")
-//             .send({
-//                 isbn: "0691161789",
-//                 amazon_url: "htt://a.co/eobPtX2",
-//                 author: "Tim Garcia",
-//                 language: "english",
-//                 pages: 264,
-//                 publisher: "Rithm University Press",
-//                 title: "Best of Uruguay",
-//                 year: 2017
-
-//             });
+//             .get("/companies");
 //         expect(response.statusCode).toBe(201);
-//         expect(response.body.book).toHaveProperty("isbn")
-//         expect(response.body.book.isbn).toEqual("0691161789");
+//         // expect(response.body.book).toHaveProperty("isbn")
+//         // expect(response.body.book.isbn).toEqual("0691161789");
 //     })
-//     // fail
-//     test("Create a invalid new book", async function () {
-//         const response = await request(app)
-//             .post("/books")
-//             .send({
-//                 isbn: "test",
-//                 amazon_url: "htt://a.co/eobPtX2",
-//                 author: "Tim Garcia",
-//                 language: "english",
-//                 pages: 264,
-//                 publisher: "Rithm University Press",
-//                 title: "Best of Uruguay",
-//                 year: 2017
+// })
 
-//             });
-//         expect(response.statusCode).toBe(400);
-//         expect(response.body.error).toHaveProperty("message");
+// describe("DELETE /companies", function () {
+
+//     test("Should remove an existing company and return a message => {message: 'Company deleted'}", async function () {
+//         const response = await request(app)
+//             .delete(`/companies/${testCompanyHandle}`);
+
+//         //test status code and response   
+//         expect(response.statusCode).toBe(201);
+//         expect(response.body).toEqual({ message: 'Company deleted' })
+
+//         //query DB for handle. Should not exist in db
+//         let testHandleResult = await db.query(`
+//         SELECT * from companies WHERE handle=$1
+//         `, [testCompanyHandle]);
+//         expect(testHandleResult.rows).toHaveLength(0);
 //     })
+// })
+
+describe("POST /companies", function () {
+
+    test("Create a new company (With valid data)", async function () {
+
+        let data = {
+            handle: "aapl",
+            name: "Apple",
+            num_employees: 2000,
+            description: "Hardware Company",
+            logo_url: "https://pmcvariety.files.wordpress.com/2018/01/amazon-logo.jpg?w=1000&h=562&crop=1"
+        }
+
+        const response = await request(app)
+            .post("/companies")
+            .send(data);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.company).toHaveProperty("handle")
+        expect(response.body.company).toEqual(data);
+
+        //added to db
+        let result = await db.query(`
+        SELECT * from companies WHERE handle=$1
+        `, [data.handle]);
+        expect(result.rows).toHaveLength(1);
+
+        //db should have two records 
+        let resultAll = await db.query(`
+        SELECT * from companies`);
+        expect(resultAll.rows).toHaveLength(2);
+    })
+
+    test("Create a new company (Without valid data)", async function () {
+
+        let data = {
+            handle: "aapl",
+            num_employees: 2000,
+            description: "Hardware Company",
+            logo_url: "https://pmcvariety.files.wordpress.com/2018/01/amazon-logo.jpg?w=1000&h=562&crop=1"
+        }
+
+        const response = await request(app)
+            .post("/companies")
+            .send(data);
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("message")
+        expect(response.body).toHaveProperty("status")
+
+        //Should not add to db
+        let result = await db.query(`
+        SELECT * from companies WHERE handle=$1
+        `, [data.handle]);
+        expect(result.rows).toHaveLength(0);
+
+        //db should have one records 
+        let resultAll = await db.query(`
+        SELECT * from companies`);
+        expect(resultAll.rows).toHaveLength(1);
+    })
+
+})
+
 
 // })
